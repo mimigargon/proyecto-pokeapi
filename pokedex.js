e3c3d2358f965e93c6c6e1e5be4dced5ab512b29
mimigargon <mireiag77@gmail.com>
2022-05-26 20:35:14 +0200

    New functions and DOM done

--- pokedex.js
+++ pokedex.js
@@ -1,41 +1,55 @@
-let arrayDePokemon = []; 
 
-const getAllPokemon = () => {
-   fetch ('https://pokeapi.co/api/v2/pokemon/?offset=0&limit=150')
-    .then (res => res.json())
-    .then (res => {
-       getAllPokemon.forEach(pokemon => {
-            fetch('https://pokeapi.co/api/v2/pokemon/')
-            .then (res => res.json())
-            .then (res => {
-                return res.result
-            })
+const getAllPokemons = () => {
+   return fetch ('https://pokeapi.co/api/v2/pokemon/?offset=0&limit=150')
+    .then ((response) => {
+        return response.json();
+    })
+    .then ((response) => {
+      return response;
+    })
+    .catch ((error) => {
+        console.log('Ha habido un error al obtener los pokemon', error);
+    });
+};
 
-            .catch(error => console.log('error ->', error));
-        }); 
-           
-            
-        
-        console.log(res)
-        return res
+const getPokemon = (url) => {
+    return fetch(url)
+    .then ((response) => {
+        return response.json();
+    })
+    .then ((response) => {
+        const pokemon = {
+            name: response.name,
+            id: response.id,
+            type: response.types.map((type) => type.type.map), 
+            image: response.sprites["front_default"], 
+        };
+        return pokemon; 
+    })
+    .catch((error) => console.log('Error obteniendo los pokemons', error));
+};
+
+const drawPokemons = (pokemons) => {
+    pokemons.forEach((pokemon) => {
+        const pokeDiv = document.createElement('div');
+        pokeDiv.classList.add('poke-card');
+        document.body.appendChild(pokeDiv);
+        const pokeImage = document.createElement('img');
+        pokeDiv.appendChild(pokeImage);
+        const pokeName = document.createElement('h3');
+        pokeDiv.appendChild(pokeName);
+        const pokeId = document.createElement('p');
+        pokeDiv.appendChild(pokeId);
+        const pokeType = document.createElement('p');
+        pokeDiv.appendChild(pokeType);
     })
 }
 
 
-getAllPokemon();
 
-
-const pokemon = results.map((result) => ({
-    name: result.name,
-    image: result.sprites['front_default'], 
-    type: result.types.map((type) => type.type.name).join(', '),
-    id: result.id
-}));
-
-const drawPokemon = (pokemons) => {
-    let gallery = document.querySelector('.container');
-    for(const pokemon of pokemons) {
-        let pokemonDiv = document.createElement('div');
-        pokemonDiv.classList.add('pokemon-card');
-    }
+const initApp = async () => {
+    const data = await getAllPokemons();
+    console.log(data);
 }
+
+initApp();
